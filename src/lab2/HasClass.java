6d5662109dcb757da4eb9aacbaf5dddad315f2aa
Zachary <Zachary@ZCS-ASUS>
2015-09-15 11:15:42 -0500

    Added interface for subclases. Second project commit.

---
+++ src/lab2/HasClass.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2;
+
+/**
+ *
+ * @author Zachary
+ */
+public interface HasClass {
+    public abstract String getCourseNumber();
+    public abstract void setCourseNumber();
+    public abstract String getCourseName();
+    public abstract void setCourseName();
+    public abstract double getCredits();
+    public abstract void setCredits();
+}
